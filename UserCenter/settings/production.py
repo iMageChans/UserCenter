0a4d1be4cfcf490f31e6e9ddc035e7f03021b94d
Chans <imagechans@gmail.com>
2025-03-17 11:32:31 +0800

    create

--- UserCenter/settings/production.py
+++ UserCenter/settings/production.py
@@ -9,7 +9,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
-                 'pocket.pulseheath.com',
+                 'pocket.nicebudgeting.com',
                  'users-service']
 
 # 数据库设置
